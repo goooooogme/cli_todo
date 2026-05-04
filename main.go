ad5a0d31e94e43a1fd1aaae8cb61c63cbed0167d
e.nefedov <e.nefedov@megar.ru>
2026-05-05 01:13:08 +0500

    add func

--- main.go
+++ main.go
@@ -8,26 +8,77 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("1 - list tasks")
-	fmt.Println("2 - add task")
-
-	fmt.Println("select operation:")
-
+func enterInput() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
 
 	input, err := reader.ReadString('\n')
 
 	input = strings.TrimSpace(input)
 
+	return input, err
+}
+
+func addTask() {
+	textForAddTask()
+
+	input, err := enterInput()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.OpenFile("tasks.txt", os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	file.WriteString(input + "\n")
+	fmt.Println("done")
+}
+
+func listTasks() {
+	file, err := os.Open("tasks.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func textForMenu() {
+	fmt.Println("1 - list tasks")
+	fmt.Println("2 - add task")
+
+	fmt.Println("select operation:")
+}
+
+func textForAddTask() {
+	fmt.Println("input")
+}
+
+func main() {
+	textForMenu()
+
+	input, err := enterInput()
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if input == "1" {
-		fmt.Println("list tasks")
+		listTasks()
 	} else if input == "2" {
-		fmt.Println("add task")
+		addTask()
 	} else {
 		fmt.Println("invalid command")
 	}
